247cb8623601ee674ee4b9651700a5bfa006de8e
Martin Atkins <mart@degeneration.co.uk>
2009-06-01 00:39:03 -0700

    An initial implementation. Kinda messy in places, but it seems to work. Needs tests.

--- lib/JSON/Streaming/Reader.pm
+++ lib/JSON/Streaming/Reader.pm
@@ -9,15 +9,475 @@ package JSON::Streaming::Reader;
 
 use strict;
 use warnings;
+use Carp;
+
+our $VERSION = '0.01';
+
+use constant ROOT_STATE => {};
+
+# Make some constants for the token types
+BEGIN {
+    foreach my $token_type (qw(start_object end_object start_array end_array start_property end_property add_string add_number add_boolean add_null error)) {
+        no strict 'refs';
+
+        my $full_name = __PACKAGE__."::".uc($token_type);
+
+        *{$full_name} = sub { $token_type };
+    }
+};
 
 sub for_stream {
     my ($class, $stream) = @_;
 
     my $self = bless {}, $class;
     $self->{stream} = $stream;
+    $self->{state} = ROOT_STATE;
+    $self->{state_stack} = [];
+    $self->{used} = 0;
+    return $self;
 }
 
-sub 
+sub get_token {
+    my ($self) = @_;
+
+    return undef if $self->{errored};
+
+    my $tok = eval {
+        my $done_comma;
+        my $need_comma = $self->made_value;
+        while (1) { # Until we find a character that's interesting
+            $self->_eat_whitespace();
+            my $char = $self->_peek_char();
+
+            unless (defined($char)) {
+                # EOF
+                die("Unexpected end of input\n") unless $self->_state == ROOT_STATE;
+                return undef;
+            }
+
+            # If we've found more stuff while we're in the root state and we've
+            # already seen stuff then there's junk at the end of the string.
+            die("Unexpected junk at the end of input") if $self->_state == ROOT_STATE && $self->{used};
+
+            if ($char eq ',' && ! $done_comma) {
+                print STDERR "It's a comma!\n";
+                if ($self->in_array || $self->in_object) {
+                    if ($self->made_value) {
+                        $self->_require_char(',');
+
+                        $done_comma = 1;
+                        next;
+                    }
+                }
+                elsif ($self->in_property) {
+                    # If we're in a property then a comma indicates
+                    # the end of the property. We exit the property state
+                    # but leave the comma so that the next get_token
+                    # can still see it.
+                    $self->_pop_state();
+                    $self->_set_made_value;
+                    return [ END_PROPERTY ];
+                }
+            }
+
+            if ($char ne '}' && $self->in_object) {
+                # If we're in an object then we must start a property here.
+                my $name_token = $self->_get_string_token();
+                die "Expected string\n" unless $name_token->[0] eq ADD_STRING;
+                my $property_name = $name_token->[1];
+                my $state = $self->_push_state();
+                $state->{in_property} = 1;
+                $self->_eat_whitespace();
+                $self->_require_char(":");
+                return [ START_PROPERTY, $property_name ];
+            }
+
+            if ($char eq '{') {
+                die("Unexpected start of object\n") unless $self->can_start_value;
+                $self->_require_char('{');
+                my $state = $self->_push_state();
+                $state->{in_object} = 1;
+                return [ START_OBJECT ];
+            }
+            elsif ($char eq '}') {
+                die("Expected another property\n") if $done_comma;
+
+                # If we're in a property then this also indicates
+                # the end of the property.
+                # We don't actually consume the } here, so the next
+                # call to get_token will see it again but it will be
+                # in the is_object state rather than is_property.
+                if ($self->in_property) {
+                    $self->_pop_state();
+                    $self->_set_made_value;
+                    return [ END_PROPERTY ];
+                }
+
+                die("End of object without matching start\n") unless $self->in_object;
+                $self->_require_char('}');
+                $self->_pop_state();
+                $self->_set_made_value();
+                return [ END_OBJECT ];
+            }
+            elsif ($char eq '[') {
+                die("Unexpected start of array\n") unless $self->can_start_value;
+                $self->_require_char('[');
+                my $state = $self->_push_state();
+                $state->{in_array} = 1;
+                return [ START_ARRAY ];
+            }
+            elsif ($char eq ']') {
+                die("End of array without matching start\n") unless $self->in_array;
+                die("Expected another value\n") if $done_comma;
+                $self->_require_char(']');
+                $self->_pop_state();
+                $self->_set_made_value();
+                return [ END_ARRAY ];
+            }
+            elsif ($char eq '"') {
+                die("Unexpected string value\n") unless $self->can_start_value;
+                die("Expected ,\n") if $need_comma && ! $done_comma;
+                return $self->_get_string_token();
+            }
+            elsif ($char eq 't') {
+                die("Unexpected boolean value\n") unless $self->can_start_value;
+                die("Expected ,\n") if $need_comma && ! $done_comma;
+                foreach my $c (qw(t r u e)) {
+                    $self->_require_char($c);
+                }
+                $self->_set_made_value();
+                return [ ADD_BOOLEAN, 1 ];
+            }
+            elsif ($char eq 'f') {
+                die("Unexpected boolean value\n") unless $self->can_start_value;
+                die("Expected ,\n") if $need_comma && ! $done_comma;
+                foreach my $c (qw(f a l s e)) {
+                    $self->_require_char($c);
+                }
+                $self->_set_made_value();
+                return [ ADD_BOOLEAN, 0 ];
+            }
+            elsif ($char eq 'n') {
+                die("Unexpected null\n") unless $self->can_start_value;
+                die("Expected ,\n") if $need_comma && ! $done_comma;
+                foreach my $c (qw(n u l l)) {
+                    $self->_require_char($c);
+                }
+                $self->_set_made_value();
+                return [ ADD_NULL ];
+            }
+            elsif ($char =~ /^[\d\-]/) {
+                die("Unexpected number value\n") unless $self->can_start_value;
+                die("Expected ,\n") if $need_comma && ! $done_comma;
+                return $self->_get_number_token();
+            }
+
+            die "Unexpected character $char\n";
+            last;
+        }
+    };
+    if ($@) {
+        $self->{errored} = 1;
+        my $error = $@;
+        chomp $error;
+        return [ ERROR, $error ];
+    }
+
+    return $tok;
+
+}
+
+# FIXME: This doesn't currently work. Blows up if there's a string in the stuff to skip,
+# which more often than not there is.
+sub skip {
+    my ($self) = @_;
+
+    my @end_chars;
+
+    @end_chars = qw(, }) if $self->in_property;
+    @end_chars = qw(}) if $self->in_object;
+    @end_chars = qw(]) if $self->in_array;
+
+    while (1) {
+        my $peek = $self->_peek_char();
+
+        die "Unexpected end of input\n" unless defined($peek);
+
+        if ($peek eq '"') {
+            # Use the normal string parser to skip over the
+            # string so that strings containing our end_chars don't
+            # cause us problems.
+            $self->_parse_string();
+        }
+
+        if (grep { $_ eq $peek } @end_chars) {
+            unless ($self->in_property && $peek eq '}') {
+                $self->_get_char();
+            }
+            $self->_pop_state();
+            return;
+        }
+        else {
+            $self->_get_char();
+        }
+    }
+
+}
+
+sub _get_char {
+    my ($self) = @_;
+
+    my $ret = $self->_peek_char();
+    $self->{peeked} = undef;
+    return $ret;
+}
+
+sub _require_char {
+    my ($self, $required) = @_;
+
+    my $char = $self->_get_char();
+    if ($char ne $required) {
+        die "Expected $required but encountered ".(defined($char) ? $char : 'EOF');
+    }
+    return $char;
+}
+
+sub _peek_char {
+    my ($self) = @_;
+
+    return $self->{peeked} if defined($self->{peeked});
+
+    my $buf = "";
+    my $success = read($self->{stream}, $buf, 1);
+
+    unless ($success) {
+        # Assume EOF
+        return undef;
+    }
+
+    return $self->{peeked} = $buf;
+}
+
+sub _eat_whitespace {
+    my ($self) = @_;
+
+    while (1) {
+        my $char = $self->_peek_char();
+
+        return if ! defined($char);
+        return if $char !~ /^\s/;
+        $self->_get_char();
+    }
+}
+
+sub _get_digits {
+    my ($self) = @_;
+
+    my $accum = "";
+
+    while ($self->_peek_char() =~ /\d/) {
+        $accum .= $self->_get_char();
+    }
+
+    # We should have got at least one digit
+    die "Expected digits but got ".$self->_peek_char() if $accum eq '';
+
+    return $accum;
+}
+
+sub _get_number_token {
+    my ($self) = @_;
+
+    my @accum = ();
+
+    if ($self->_peek_char() eq '-') {
+        push @accum, $self->_get_char();
+    }
+
+    push @accum, $self->_get_digits;
+
+    if ($self->_peek_char() eq '.') {
+        push @accum, $self->_get_char();
+
+        push @accum, $self->_get_digits;
+
+    }
+
+    if ($self->_peek_char() =~ /e/i) {
+        push @accum, $self->_get_char();
+
+        my $peek = $self->_peek_char();
+
+        if ($peek eq '+' || $peek eq '-') {
+            push @accum, $self->_get_char();
+        }
+
+        push @accum, $self->_get_digits;
+    }
+
+    $self->_set_made_value();
+
+    # Join and convert to number
+    # Perl's numberification conveniently does what we need here.
+    return [ ADD_NUMBER, join('', @accum)+0 ];
+}
+
+my %escape_chars = (
+    b => "\b",
+    f => "\f",
+    n => "\n",
+    r => "\r",
+    t => "\t",
+    "\\" => "\\",
+    "/" => "/",
+    '"' => '"',
+);
+
+sub _get_string_token {
+    my ($self) = @_;
+
+    $self->_require_char('"');
+
+    my $accum = "";
+
+    while (1) {
+        my $char = $self->_get_char();
+
+        die "Unterminated string\n" unless defined($char);
+        if ($char eq '"') {
+            last;
+        }
+
+        if ($char eq "\\") {
+            my $escape_char = $self->_get_char();
+
+            die "Unfinished escape sequence\n" unless defined($escape_char);
+
+            if (my $replacement = $escape_chars{$escape_char}) {
+                $accum .= $replacement;
+            }
+            elsif ($escape_char eq 'u') {
+                # TODO: Support this
+                die "\\u sequence not yet supported\n";
+            }
+            else {
+                die "Invalid escape sequence \\$escape_char";
+            }
+        }
+        else {
+            $accum .= $char;
+        }
+    }
+
+    $self->_set_made_value();
+
+    return [ ADD_STRING, $accum ];
+}
+
+sub _parse_string {
+    my ($self) = @_;
+
+    $self->_require_char('"');
+
+    my $accum = "";
+
+    # Don't bother building the result buffer if we're called in void context
+    my $want_result = defined(wantarray());
+
+    while (1) {
+        my $char = $self->_get_char();
+
+        die "Unterminated string\n" unless defined($char);
+        if ($char eq '"') {
+            last;
+        }
+
+        if ($char eq "\\") {
+            my $escape_char = $self->_get_char();
+
+            die "Unfinished escape sequence\n" unless defined($escape_char);
+
+            if (my $replacement = $escape_chars{$escape_char}) {
+                $accum .= $replacement if $want_result;
+            }
+            elsif ($escape_char eq 'u') {
+                # TODO: Support this
+                die "\\u sequence not yet supported\n";
+            }
+            else {
+                die "Invalid escape sequence \\$escape_char";
+            }
+        }
+        else {
+            $accum .= $char if $want_result;
+        }
+    }
+
+    return $accum;
+}
+
+sub _push_state {
+    my ($self) = @_;
+
+    Carp::croak("Can't add anything else: JSON output is complete") if $self->_state == ROOT_STATE && $self->{used};
+
+    $self->{used} = 1;
+
+    push @{$self->{state_stack}}, $self->{state};
+
+    $self->{state} = {
+        in_object => 0,
+        in_array => 0,
+        in_property => 0,
+        made_value => 0,
+    };
+
+    return $self->{state};
+}
+
+sub _pop_state {
+    my ($self) = @_;
+
+    my $state = pop @{$self->{state_stack}};
+    return $self->{state} = $state;
+}
+
+sub _state {
+    my ($self) = @_;
+
+    return $self->{state};
+}
+
+sub in_object {
+    return $_[0]->_state->{in_object} ? 1 : 0;
+}
+
+sub in_array {
+    return $_[0]->_state->{in_array} ? 1 : 0;
+}
+
+sub in_property {
+    return $_[0]->_state->{in_property} ? 1 : 0;
+}
+
+sub made_value {
+    return $_[0]->_state->{made_value} ? 1 : 0;
+}
+
+sub _set_made_value {
+    $_[0]->_state->{made_value} = 1 unless $_[0]->_state == ROOT_STATE;
+}
+
+sub can_start_value {
+
+    return 0 if $_[0]->in_property && $_[0]->made_value;
+
+    return $_[0]->in_object ? 0 : 1;
+}
+
+sub _expecting_property {
+    return $_[0]->in_object ? 1 : 0;
+}
 
 1;
 
@@ -84,6 +544,8 @@ and have C<add_> prefixes.
 For convenience the token type names match the method names used in the "raw" API
 of L<JSON::Streaming::Writer>, so it is straightforward to implement a streaming JSON
 normalizer by feeding the output from this module into the corresponding methods on that module.
+However, this module does have an additional special token type 'error' which is used
+to indicate tokenizing errors and does not have a corresponding method on the writer.
 
 =head2 start_object, end_object
 
@@ -117,6 +579,10 @@ Represents a JSON boolean. If it's C<true> then 1 is passed as an argument, or i
 =head2 add_null
 
 Represents a JSON null.
+
+=head2 error($string)
+
+Indicates a tokenization error. A human-readable description of the error is included in $string.
 
 =head1 STREAM BUFFERING
 
